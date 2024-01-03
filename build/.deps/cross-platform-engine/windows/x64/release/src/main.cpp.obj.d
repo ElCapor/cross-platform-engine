1a11a5dfe52bf7130246271c6c400b257dadc675
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-01-04 00:00:51 +0100

    remove

--- build/.deps/cross-platform-engine/windows/x64/release/src/main.cpp.obj.d
+++ build/.deps/cross-platform-engine/windows/x64/release/src/main.cpp.obj.d
@@ -1,35 +1,39 @@
 {
-    depfiles_cl_json = "{\
-    \"Version\": \"1.2\",\
-    \"Data\": {\
-        \"Source\": \"c:\\\\users\\\\administrator\\\\desktop\\\\raylib\\\\cross-platform-engine\\\\src\\\\main.cpp\",\
-        \"ProvidedModule\": \"\",\
-        \"Includes\": [\
-            \"c:\\\\users\\\\administrator\\\\desktop\\\\raylib\\\\cross-platform-engine\\\\build\\\\.packages\\\\r\\\\raylib\\\\latest\\\\8574311f4b544fa89a3d8c3ee79d1929\\\\include\\\\raylib.h\",\
-            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.37.32822\\\\include\\\\stdarg.h\",\
-            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.37.32822\\\\include\\\\vcruntime.h\",\
-            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.37.32822\\\\include\\\\sal.h\",\
-            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.37.32822\\\\include\\\\concurrencysal.h\",\
-            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.37.32822\\\\include\\\\vadefs.h\",\
-            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.37.32822\\\\include\\\\stdbool.h\"\
-        ],\
-        \"ImportedModules\": [],\
-        \"ImportedHeaderUnits\": []\
-    }\
-}",
-    files = {
-        [[src\main.cpp]]
-    },
     values = {
-        [[C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.37.32822\bin\HostX64\x64\cl.exe]],
+        [[C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.38.33130\bin\HostX64\x64\cl.exe]],
         {
             "-nologo",
             "-O2",
             "-fp:fast",
             "/EHsc",
             "-external:W0",
-            [[-external:IC:\Users\Administrator\Desktop\Raylib\cross-platform-engine\build\.packages\r\raylib\latest\8574311f4b544fa89a3d8c3ee79d1929\include]],
+            [[-external:IC:\Users\HP\Documents\GitHub\cross-platform-engine\build\.packages\r\raylib\latest\8574311f4b544fa89a3d8c3ee79d1929\include]],
+            "-external:W0",
+            [[-external:IC:\Users\HP\AppData\Local\.xmake\packages\r\raygui\latest\3599f6b167d844bb97300d9682a51c11\include]],
+            "-external:W0",
+            [[-external:IC:\Users\HP\Documents\GitHub\cross-platform-engine\build\.packages\r\raylib\5.x\8574311f4b544fa89a3d8c3ee79d1929\include]],
             "-DNDEBUG"
         }
-    }
+    },
+    files = {
+        [[src\main.cpp]]
+    },
+    depfiles_cl_json = "{\
+    \"Version\": \"1.2\",\
+    \"Data\": {\
+        \"Source\": \"c:\\\\users\\\\hp\\\\documents\\\\github\\\\cross-platform-engine\\\\src\\\\main.cpp\",\
+        \"ProvidedModule\": \"\",\
+        \"Includes\": [\
+            \"c:\\\\users\\\\hp\\\\documents\\\\github\\\\cross-platform-engine\\\\build\\\\.packages\\\\r\\\\raylib\\\\latest\\\\8574311f4b544fa89a3d8c3ee79d1929\\\\include\\\\raylib.h\",\
+            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.38.33130\\\\include\\\\stdarg.h\",\
+            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.38.33130\\\\include\\\\vcruntime.h\",\
+            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.38.33130\\\\include\\\\sal.h\",\
+            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.38.33130\\\\include\\\\concurrencysal.h\",\
+            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.38.33130\\\\include\\\\vadefs.h\",\
+            \"c:\\\\program files\\\\microsoft visual studio\\\\2022\\\\community\\\\vc\\\\tools\\\\msvc\\\\14.38.33130\\\\include\\\\stdbool.h\"\
+        ],\
+        \"ImportedModules\": [],\
+        \"ImportedHeaderUnits\": []\
+    }\
+}"
 }
--- xmake.lua
+++ xmake.lua
@@ -10,12 +10,45 @@ package("raylib")
         end)
 package_end()
 
+package("raygui")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/raysan5/raygui")
+    set_description("A simple and easy-to-use immediate-mode gui library")
+    set_license("zlib")
+    add_deps("raylib 5.x")
+    add_configs("implemention", { description = "Define implemention.", default = false, type = "boolean"})
+
+    on_load(function (package)
+        if package:config("implemention") then
+            package:add("defines", "RAYGUI_IMPLEMENTATION")
+        end
+    end)
+
+    on_install("windows", "linux", "macosx", function (package)
+        os.cp("src/*", package:installdir("include"))
+    end)
+package_end()
+
 add_requires("raylib")
+add_requires("raygui")
 
 target("cross-platform-engine") do
+	target("cross-platform-engine") do
 	set_kind("binary")
 	set_optimize("fastest")
     add_files("src/*.cpp")
     add_headerfiles("include/*.h")
+    
 	add_packages("raylib")
+    add_packages("raygui")
+
+    if is_plat("macosx") then
+        add_frameworks("CoreVideo", "CoreGraphics", "AppKit", "IOKit", "CoreFoundation", "Foundation")
+    elseif is_plat("windows", "mingw") then
+        add_syslinks("gdi32", "user32", "winmm", "shell32")
+    elseif is_plat("linux") then
+        add_syslinks("pthread", "dl", "m")
+        add_deps("libx11", "libxrandr", "libxrender", "libxinerama", "libxcursor", "libxi", "libxfixes", "libxext")
+    end
+end
 end
