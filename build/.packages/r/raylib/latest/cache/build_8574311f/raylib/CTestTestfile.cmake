8bc2f1fc40aa1b8bdc48a1d7fe57daf5f957346a
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-01-05 20:21:23 +0100

    Restart

--- build/.packages/r/raylib/latest/cache/build_8574311f/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Administrator/Desktop/Raylib/cross-platform-engine/raylib/src
-# Build directory: C:/Users/Administrator/Desktop/Raylib/cross-platform-engine/build/.packages/r/raylib/latest/cache/build_8574311f/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
